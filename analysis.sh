3d3dc9111e6b3ec46f61f8258661d6c76aa0e526
tregedron <polovms@gmail.com>
2023-06-14 21:44:19 +0300

    analysis.sh fix

--- analysis.sh
+++ analysis.sh
@@ -1,6 +1,6 @@
 xtc_file=$1
 
-window=4
+window=10000
 shift=1
 
 xtc_file=$(basename "${xtc_file%.xtc}")
@@ -8,3 +8,4 @@ xtc_file=$(basename "${xtc_file%.xtc}")
 source venv/bin/activate
 
 python main.py -trj "data/${xtc_file}.xtc" -top "data/${xtc_file}.gro" -w "${window}" -s "${shift}" > "logs_536only/${xtc_file}_${window}_${shift}"
+
