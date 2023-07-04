04f149ae48ad43527586e6bc0a2c0858ef264a5d
tregedron <polovms@gmail.com>
2023-07-04 23:22:26 +0300

    Sh scripts small changes

--- analysis.sh
+++ analysis.sh
@@ -1,11 +1,13 @@
 xtc_file=$1
+results=$2
+data_path=$3
 
-window=30000
+window=15000
 shift=1
 
 xtc_file=$(basename "${xtc_file%.xtc}")
 
 source venv/bin/activate
 
-python main.py -trj "data/${xtc_file}.xtc" -top "data/${xtc_file}.gro" -w "${window}" -s "${shift}" > "logs/${xtc_file}_${window}_${shift}"
+python main.py -trj "${data_path}/${xtc_file}.xtc" -top "${data_path}/${xtc_file}.gro" -w "${window}" -s "${shift}" -res "${results}" > "logs/${xtc_file}_${window}_${shift}"
 
